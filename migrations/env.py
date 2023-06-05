de2de6e771c85f8f0afae45d3030def1cbb436f9
Vadim <yagofarov.vadim@mail.ru>
2023-06-05 13:34:28 +0500

    begin migrations

--- migrations/env.py
+++ migrations/env.py
@@ -1,16 +1,23 @@
+import os
+import sys
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context
+sys.path.append(os.path.join(sys.path[0], 'src'))
+
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from src.type_device.model import metadata as metadata_type_device
+from src.devices.model import metadata as metadata_devices
+from src.drivers.model import metadata as metadata_drivers
+from src.quantity_products.model import metadata as metadata_quantity_products
+from src.rmk.model import metadata as metadata_rmk
+from src.store.model import metadata as metadata_store
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-
-from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from src.store.model import metadata
-
 config = context.config
 
 section = config.config_ini_section
@@ -29,7 +36,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [metadata_type_device, metadata_devices, metadata_drivers, metadata_quantity_products, metadata_rmk,
+                   metadata_store]
 
 
 # other values from the config, defined by the needs of env.py,
